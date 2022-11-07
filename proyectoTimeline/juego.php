8ea7b6522a5078dba5f387f63f42b48e1e85665e
Alejandro Wisan Berro <56206027+Berr0@users.noreply.github.com>
2022-11-07 17:49:32 +0100

    update

---
+++ proyectoTimeline/juego.php
@@ -0,0 +1,22 @@
+<html>
+
+<?php
+    var_export($_POST);
+    $Mazo[] = new Cartas(1, 'Mondongo')
+?>
+
+<head>
+<title>  Timeline </title>
+<style>
+
+</style>
+
+</head>
+<body>
+
+
+
+
+</body>
+
+</html>
